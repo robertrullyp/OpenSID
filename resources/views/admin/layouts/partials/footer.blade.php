fba21f84af099b40e20fbc09f0fbe28caba6cc49
Robert Rully Pernando <robert.rully@gmail.com>
2025-11-19 12:50:27 +0800

    update admin footer

--- resources/views/admin/layouts/partials/footer.blade.php
+++ resources/views/admin/layouts/partials/footer.blade.php
@@ -3,5 +3,7 @@
         <b>Versi</b> {{ AmbilVersi() }}
     </div>
     <strong>Aplikasi <a href="https://github.com/OpenSID/OpenSID" target="_blank">
-            <?= config_item('nama_aplikasi') ?></a>, dikembangkan oleh <a href="<?= config_item('fb_opendesa') ?>" target="_blank">Komunitas <?= config_item('nama_aplikasi') ?></a>.</strong>
+            <?= config_item('nama_aplikasi') ?></a> by <a href="<?= config_item('fb_opendesa') ?>" target="_blank">Komunitas <?= config_item('nama_aplikasi') ?></a>,
+            dikembangkan dan dimodifikasi oleh <a href="https://kim-kapul.kim.id/" target="_blank">Komunitas Informasi Masyarakat (KIM) Kapul</a>.
+    </strong>
 </footer>
